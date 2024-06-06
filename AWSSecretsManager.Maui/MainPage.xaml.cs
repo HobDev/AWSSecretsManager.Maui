74efd51d47903431e1d67322e7dccbb611ad2061
Paramjit Singh <sparamjit92@gmail.com>
2024-06-06 18:24:58 +0530

    using Caching Library for .NET

--- AWSSecretsManager.Maui/MainPage.xaml.cs
+++ AWSSecretsManager.Maui/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using Amazon.SecretsManager;
 using Amazon;
 using Amazon.Runtime;
+using Amazon.SecretsManager.Extensions.Caching;
 
 
 namespace AWSSecretsManager.Maui
@@ -17,49 +18,36 @@ namespace AWSSecretsManager.Maui
 
         private async void  OnLoaded(object? sender, EventArgs e)
         {
-          string secret1=await  GetSecretFromAWS();
-           
-            string secret2 = "Secret 2";
-            string secret3 = "Secret 3";
+            string secretName = Secrets.SecretName;
+            string region = Secrets.Region;
 
-            Label1.Text = $"Secret1:  {secret1}";
-            Label2.Text = $"Secret2:  {secret2}";
-            Label3.Text = $"Secret3:  {secret3}";
-
-        }
-
-         private async Task<string> GetSecretFromAWS()
-        {
-            /*    *	Use this code snippet in your app.
-    *	If you need more information about configurations or implementing the sample code, visit the AWS docs:
-    *	https://aws.amazon.com/developer/language/net/getting-started
-    */
-            string secretName = "prod/DomeMobile/MongoDBAppId";
-            string region = "ca-central-1";
-
-            AWSCredentials credentials = new BasicAWSCredentials( Secrets.AccessKey, Secrets.SecretKey);
-            IAmazonSecretsManager client = new AmazonSecretsManagerClient(credentials,RegionEndpoint.GetBySystemName(region));
-
-            GetSecretValueRequest request = new GetSecretValueRequest
+            AWSCredentials credentials = new BasicAWSCredentials(Secrets.AccessKey, Secrets.SecretKey);
+            IAmazonSecretsManager amazonSecretsManager = new AmazonSecretsManagerClient(credentials, RegionEndpoint.GetBySystemName(region));
+            SecretCacheConfiguration cacheConfig = new SecretCacheConfiguration
             {
-                SecretId = secretName,
-                VersionStage = "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified.
+                // the TTL cache refresh duration to 24 hours
+                CacheItemTTL = 86400000
             };
 
-            GetSecretValueResponse response;
+            SecretsManagerCache cache = new SecretsManagerCache(amazonSecretsManager, cacheConfig);
+
+            string response = string.Empty;
 
             try
             {
-                response = await client.GetSecretValueAsync(request);
+                response = await cache.GetSecretString("secretName");
+
             }
             catch (Exception ex)
             {
-                // For a list of the exceptions thrown, see
-                // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-                throw ex;
+                await Shell.Current.DisplayAlert("Error", ex.Message, "OK");
             }
 
-            return response.SecretString;
+            
+
+            Label1.Text=$"Secret= {response}";  
         }
+
+        
     }
 }
